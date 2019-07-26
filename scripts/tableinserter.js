40c680093ab9643c9eba34de7b4ed25df5ca4d58
GaurangTandon <1gaurangtandon@gmail.com>
2019-07-26 16:36:23 +0530

    my 2nd commit :)

---
+++ scripts/tableinserter.js
@@ -0,0 +1,14 @@
+const table = document.getElementById("grid"),
+    TABLE_SIZE = [6, 7];
+
+for (let row = 0; row < TABLE_SIZE[0]; row++) {
+    const tr = document.createElement("tr");
+    for (let col = 0; col < TABLE_SIZE[1]; col++) {
+        const td = document.createElement("td");
+        td.className = "col";
+        const button = document.createElement("button");
+        td.appendChild(button);
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
